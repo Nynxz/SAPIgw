1bc910ab9df980aa014892ef2892503e36cb66cf
Henry Lee <nynxzx@gmail.com>
2024-07-01 15:25:41 +1000

    modify ratelimit middleware

--- src/middleware/ratelimit_mw.ts
+++ src/middleware/ratelimit_mw.ts
@@ -2,45 +2,41 @@ import rateLimit from "express-rate-limit";
 //TODO: make this better...
 import { NextFunction, Request, Response } from "express";
 
-let pingRateLimitReached = (req: Request, res: Response) => {
-  console.log(`A rate limit has been reached on ${req.path}... investigate?`);
-  console.log(req);
-  res.status(429).send("Too many requests, please try again later.");
+type LimiterConfig = {
+  windowMinutes: number;
+  limit: number;
+  pathsToSkip?: string[];
 };
+const RateLimitMW = ({
+  windowMinutes,
+  limit,
+  pathsToSkip = [],
+}: LimiterConfig) => {
+  const _pingRateLimitReached = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.log(`A rate limit has been reached on ${req.path}... investigate?`);
+    res.status(429).send("Too many requests, please try again later.");
+  };
 
-let pingRateLimitReachedHello = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log(`A rate limit has been reached on ${req.path}... investigate?`);
-  res.status(429).send("Too many requests, please try again later.");
-};
-
-const limiter2 = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
-  handler: pingRateLimitReached,
-  skip: (req, res) => {
-    if (req.path == "/hello") {
-      return true;
-    } else {
+  const _skipFunc =
+    (_pathsToSkip: string[]) => (req: Request, res: Response) => {
+      if (req.path in _pathsToSkip) {
+        return true;
+      }
       return false;
-    }
-  },
-});
+    };
 
-const limiter30 = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 60, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  return rateLimit({
+    windowMs: windowMinutes * 60 * 1000,
+    limit,
+    standardHeaders: "draft-7", // IDK WTF this means?
+    legacyHeaders: false,
+    handler: _pingRateLimitReached,
+    skip: _skipFunc(pathsToSkip),
+  });
+};
 
-  // store: ... , // Redis, Memcached, etc. See below.
-  handler: pingRateLimitReachedHello,
-});
-
-export { limiter2, limiter30 };
+export { RateLimitMW };
